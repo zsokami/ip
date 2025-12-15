e488d63dad65953f24948dd90065fbf085034c1d
钟孟捷 <718335940@qq.com>
2025-12-16 04:19:07 +0800

    add /geo; add /down

--- netlify/edge-functions/main.ts
+++ netlify/edge-functions/main.ts
@@ -19,6 +19,43 @@ const ROUTES: Route[] = [
       return new Response(ctx.ip)
     },
   },
+  {
+    pattern: new URLPattern({ pathname: '/geo' }),
+    handler(_req, _match, { ip, geo, server }) {
+      return Response.json({ ip, ...geo, serverregion: server.region })
+    },
+  },
+  {
+    pattern: new URLPattern({ pathname: '/down/:n(\\d+):unit([km])?' }),
+    handler(_req, { pathname: { groups } }) {
+      let n = Number(groups.n) * (!groups.unit ? 1 : groups.unit === 'k' ? 1024 : 1024 * 1024)
+      if (!(n <= 20 * 1024 * 1024)) return new Response('Not Found', { status: 404 })
+      const bufSize = 65536
+      const buf = new Uint8Array(bufSize).fill(97)
+      return new Response(
+        new ReadableStream({
+          pull(controller) {
+            if (controller.desiredSize === null) return
+            do {
+              if ((n -= bufSize) >= 0) {
+                controller.enqueue(buf)
+              } else {
+                controller.enqueue(buf.subarray(0, n))
+                controller.close()
+                return
+              }
+            } while (controller.desiredSize > 0)
+          },
+        }),
+        {
+          headers: {
+            'content-type': 'application/octet-stream',
+            'content-length': `${n}`,
+          },
+        },
+      )
+    },
+  },
 ]
 
 export default (req: Request, ctx: Context) => {
